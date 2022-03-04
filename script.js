a527afea6fbd32ccdedeb57a5c0e46e27d01c9c2
Kebron Solomon Z <keb2@macbook-pro.lan>
2022-03-04 22:53:47 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/*
+1. Roll dice and display it
+ - random dice roll
+      - random number between 1-6
+      - switch statement to connect random number with relevant Img
+
+-  if(dice !== 1)
+      - diceRoll + currentScore = NewScore -> display
+
+- if(dice == 1)
+      - switch player
+      - add newScores to a new variable 
+      - highlight ui 
+
+2. User holds 
+  -  current score + total score >= 100  
+
+*/
+
