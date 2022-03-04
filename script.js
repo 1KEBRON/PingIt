5a3702167f75053122103e4d138769e5fbeb8e46
Kebron Solomon Z <keb2@macbook-pro.lan>
2022-03-04 23:56:35 +0100

    add dice roll

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
 'use strict';
 
+const score0E = document.querySelector('#score--0')
+const score1E = document.querySelector('#score--1');
+const diceE = document.querySelector('.dice')
+const rollDiceBtn = document.querySelector('.btn--roll');
+const restartBtn = document.querySelector('.btn--new');
+const holdDiceBtn = document.querySelector('.btn--hold');
+
+// starting conditions
+score0E.textContent = 0;
+score1E.textContent = 0;
+diceE.classList.add('hidden');
+const randomDice = Math.trunc(Math.random() * 6)+1
+console.log(randomDice);
+
+const handleDiceRoll = ()=>{
+      const randomDice = Math.trunc(Math.random() * 6) + 1;
+      diceE.classList.remove('hidden');
+
+      switch (randomDice) {
+      case 1:
+            diceE.src = '/dice-1.png'
+            break;
+      case 2: 
+            diceE.src = '/dice-2.png'
+            break;
+      case 3:
+            diceE.src = '/dice-3.png'
+            break;
+      case 4: 
+            diceE.src = '/dice-4.png';
+            break
+      case 5: 
+            diceE.src = '/dice-5.png';
+            break
+      case 6:
+            diceE.src = '/dice-6.png'
+        default:
+          break;
+      }
+}
+rollDiceBtn.addEventListener('click',handleDiceRoll)
 /*
 1. Roll dice and display it
  - random dice roll
