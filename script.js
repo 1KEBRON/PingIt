9935ccfdb26f663306be2fa6b5406f8f44f64512
Kebron Solomon Z <keb2@macbook-pro.lan>
2022-03-05 14:31:36 +0100

    add active player switch logic w/ui

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
 'use strict';
 
+// Selecting elements 
+const player0E = document.querySelector('.player--0');
+const player1E = document.querySelector('.player--1');
+
 const score0E = document.querySelector('#score--0')
 const score1E = document.querySelector('#score--1');
+
+const currentScore0E = document.querySelector('#current--0');
+const currentScore1E = document.querySelector('#current--1');
+
 const diceE = document.querySelector('.dice')
 const rollDiceBtn = document.querySelector('.btn--roll');
 const restartBtn = document.querySelector('.btn--new');
 const holdDiceBtn = document.querySelector('.btn--hold');
 
 // starting conditions
+let score = [0,0]
 score0E.textContent = 0;
 score1E.textContent = 0;
+let currentScore = 0;
+let activePlayer = 0
 diceE.classList.add('hidden');
-const randomDice = Math.trunc(Math.random() * 6)+1
-console.log(randomDice);
+
 
 const handleDiceRoll = ()=>{
+      // 1. random dice roll 
       const randomDice = Math.trunc(Math.random() * 6) + 1;
+      // 2. display dice 
       diceE.classList.remove('hidden');
-
-      switch (randomDice) {
-      case 1:
-            diceE.src = '/dice-1.png'
-            break;
-      case 2: 
-            diceE.src = '/dice-2.png'
-            break;
-      case 3:
-            diceE.src = '/dice-3.png'
-            break;
-      case 4: 
-            diceE.src = '/dice-4.png';
-            break
-      case 5: 
-            diceE.src = '/dice-5.png';
-            break
-      case 6:
-            diceE.src = '/dice-6.png'
-        default:
-          break;
+      diceE.src = `/dice-${randomDice}.png`
+      console.log(randomDice)
+      // 3. if dice == 1 
+      if(randomDice !== 1 ){
+            currentScore += randomDice
+            document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+      }//switch players
+      else{
+            document.querySelector( `#current--${activePlayer}` ).textContent = 0;
+            currentScore = 0
+            activePlayer =  activePlayer === 0 ? 1 : 0
+            player0E.classList.toggle('player--active');
+            player1E.classList.toggle('player--active')
       }
+
 }
 rollDiceBtn.addEventListener('click',handleDiceRoll)
 /*
